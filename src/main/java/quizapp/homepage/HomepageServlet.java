8cf43a3d56abec45d98c5b5bd46f12a67d7250fd
atsin21 <atsin21@freeuni.edu.ge>
2023-08-06 22:49:22 +0400

    create homepage servlet for landing page after login

--- src/main/java/quizapp/homepage/HomepageServlet.java
+++ src/main/java/quizapp/homepage/HomepageServlet.java
@@ -1,6 +1,7 @@
 package quizapp.homepage;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,13 +9,14 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
+@WebServlet(name = "homepageServlet", value = "/secured/homepage")
 public class HomepageServlet  extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
+        HttpSession session = req.getSession(false);
 
         if (session == null || !(boolean) session.getAttribute("AUTHENTICATED")) {
-
+            resp.sendRedirect("/auth/login");
         }
 
         req.getRequestDispatcher("/homepage.jsp").forward(req, resp);
