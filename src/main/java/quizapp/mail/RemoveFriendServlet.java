c567a330d004647044e7a1ae537dd1c132bd352c
atsin21 <atsin21@freeuni.edu.ge>
2023-08-07 19:06:48 +0400

    add dedicated servlet for removing friend

---
+++ src/main/java/quizapp/mail/RemoveFriendServlet.java
@@ -0,0 +1,29 @@
+package quizapp.mail;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import quizapp.managers.FriendManager;
+import quizapp.models.domain.User;
+
+import java.io.IOException;
+
+@WebServlet(name = "removeFriendServlet", value = "/removefriend")
+public class RemoveFriendServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User currentUser = (User) session.getAttribute("user");
+
+        String username_1 = currentUser.getUsername();
+        String username_2 = req.getParameter("friendToRemove");
+
+        FriendManager friendManager = (FriendManager) req.getServletContext().getAttribute("friendManager");
+        friendManager.removeFriends(username_1, username_2);
+
+        resp.sendRedirect("/secured/homepage");
+    }
+}
