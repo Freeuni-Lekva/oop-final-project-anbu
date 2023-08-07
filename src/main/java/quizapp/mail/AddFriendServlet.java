5181591c943d7ee462e2614dd9d3acf733af1442
atsin21 <atsin21@freeuni.edu.ge>
2023-08-07 19:06:17 +0400

    add dedicated servlet for adding friend functionality

---
+++ src/main/java/quizapp/mail/AddFriendServlet.java
@@ -0,0 +1,30 @@
+package quizapp.mail;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import quizapp.managers.FriendManager;
+import quizapp.models.domain.User;
+
+import java.io.IOException;
+
+@WebServlet(name = "addFriendServlet", value = "/addfriend")
+public class AddFriendServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        FriendManager friendManager = (FriendManager) req.getServletContext().getAttribute("friendManager");
+
+        HttpSession session = req.getSession(false);
+        User currentUser = (User) session.getAttribute("user");
+
+        String receiver = currentUser.getUsername();
+        String sender = req.getParameter("sender");
+
+        friendManager.makeFriends(sender, receiver);
+
+        resp.sendRedirect("/secured/homepage");
+    }
+}
