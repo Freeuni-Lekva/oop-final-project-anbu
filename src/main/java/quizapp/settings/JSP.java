481c84bf39ef3a1a01bef9f904ce68267ab592bc
atsin21 <atsin21@freeuni.edu.ge>
2023-08-17 23:51:09 +0400

    create separate file for jsp file locations specification

---
+++ src/main/java/quizapp/settings/JSP.java
@@ -0,0 +1,12 @@
+package quizapp.settings;
+
+public class JSP {
+
+    /****************************** JSP Endpoint Specification ********************************/
+    public static final String LOGIN = "/Auth/login.jsp";
+    public static final String REGISTER = "/Auth/register.jsp";
+    public static final String REGISTER_ERROR = "/Auth/registerError.jsp";
+
+    public static final String HOMEPAGE = "/homepage.jsp";
+    public static final String USER = "/user.jsp";
+}
