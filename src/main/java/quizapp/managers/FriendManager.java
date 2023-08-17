5cd0732bf72848605e07a145afb8ee04d58b7276
atsin21 <atsin21@freeuni.edu.ge>
2023-08-17 16:35:13 +0400

    add some checking methods

--- src/main/java/quizapp/managers/FriendManager.java
+++ src/main/java/quizapp/managers/FriendManager.java
@@ -150,4 +150,47 @@ public class FriendManager {
         return friendList;
     }
 
+    public boolean friendRequestIsSent(String from, String to) {
+        String sql = "select * from friend_requests where sender = ? and receiver = ?";
+
+        try (Connection conn = _source.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, from);
+            ps.setString(2, to);
+
+            MyLogger.info(ps.toString());
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
+    public boolean areFriends(String username_1, String username_2) {
+        String sql = "select * from friends where (username_1 = ? and username_2 = ?) or (username_1 = ? and username_2 = ?)";
+
+        try (Connection conn = _source.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, username_1);
+            ps.setString(2, username_2);
+            ps.setString(3, username_2);
+            ps.setString(4, username_1);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
 }
