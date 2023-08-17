644bab2bef6385eccf160470e8b06f4a97bc4e56
atsin21 <atsin21@freeuni.edu.ge>
2023-08-17 23:57:32 +0400

    remove unused import

--- src/main/java/quizapp/models/domain/message/FriendRequest.java
+++ src/main/java/quizapp/models/domain/message/FriendRequest.java
@@ -1,7 +1,5 @@
 package quizapp.models.domain.message;
 
-import quizapp.models.domain.User;
-
 public class FriendRequest extends Message {
     public FriendRequest(String sender, String receiver) {
         super(sender, receiver, MessageType.FRIEND);
