2d345cae6d997f7315e53b6560b35b8b5ee888e9
atsin21 <atsin21@freeuni.edu.ge>
2023-08-21 06:29:17 +0400

    add functions for friends and users activities and some minor modifications

--- src/main/java/quizapp/managers/HistoryManager.java
+++ src/main/java/quizapp/managers/HistoryManager.java
@@ -1,12 +1,11 @@
 package quizapp.managers;
 
+import quizapp.models.domain.QuizActivity;
 import quizapp.models.domain.QuizHistory;
+import quizapp.models.questions.Quiz;
 import utils.DatabaseConnectionSource;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +17,8 @@ public class HistoryManager {
         this._source = DatabaseConnectionSource.getInstance();
     }
 
-    public void addEntry(String username, int quiz_id, int score, int completion_time) {
-        String sql = "insert into quiz_history(username, quiz_id, score, completion_time) values(?, ?, ?, ?)";
+    public void addEntry(String username, int quiz_id, int score, int completion_time_seconds) {
+        String sql = "insert into quiz_history(username, quiz_id, score, completion_time_seconds) values(?, ?, ?, ?)";
 
         try (Connection conn = _source.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
@@ -27,7 +26,7 @@ public class HistoryManager {
             ps.setString(1, username);
             ps.setInt(2, quiz_id);
             ps.setInt(3, score);
-            ps.setInt(4, completion_time);
+            ps.setInt(4, completion_time_seconds);
 
             ps.execute();
         } catch (SQLException e) {
@@ -36,7 +35,7 @@ public class HistoryManager {
     }
 
     public List<QuizHistory> getAllEntries() {
-        String sql = "select * from quiz_history";
+        String sql = "select * from quiz_history qh join quizzes q on qh.quiz_id = q.quiz_id;";
 
         List<QuizHistory> entries = new ArrayList<>();
 
@@ -49,9 +48,10 @@ public class HistoryManager {
                int quiz_id = rs.getInt("quiz_id");
                String user = rs.getString("username");
                int score = rs.getInt("score");
-               int completion_time = rs.getInt("completion_time");
+               int completion_time_seconds = rs.getInt("completion_time_seconds");
+               String quiz_name = rs.getString("quiz_name");
 
-               QuizHistory entry = new QuizHistory(user, quiz_id, score, completion_time);
+               QuizHistory entry = new QuizHistory(user, quiz_id, score, completion_time_seconds, quiz_name);
 
                entries.add(entry);
            }
@@ -62,4 +62,77 @@ public class HistoryManager {
 
         return entries;
     }
+
+    public List<QuizHistory> getFriendActivity(String username) {
+        FriendManager friendManager = new FriendManager();
+
+        List<String> friends = friendManager.getFriends(username);
+
+        StringBuilder queryBuilder = new StringBuilder("SELECT * FROM quiz_history qh join quizzes q on qh.quiz_id = q.quiz_id WHERE");
+
+        for (String friend : friends) {
+            queryBuilder.append(String.format(" (qh.username = '%s') OR", friend));
+        }
+
+        queryBuilder.append(" (1 = 0) order by completion_date desc;");
+
+        String sql = queryBuilder.toString();
+
+        List<QuizHistory> activities = new ArrayList<>();
+
+        try (Connection conn = _source.getConnection();
+             Statement stmt = conn.createStatement();) {
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                String user = rs.getString("username");
+                int score = rs.getInt("score");
+                int quiz_id = rs.getInt("quiz_id");
+                int completion_time_seconds = rs.getInt("completion_time_seconds");
+                String quiz_name = rs.getString("quiz_name");
+
+                QuizHistory quizActivity = new QuizHistory(user, score, quiz_id, completion_time_seconds, quiz_name);
+
+                activities.add(quizActivity);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return activities;
+    }
+
+    public List<QuizHistory> getUserActivity(String username) {
+        String sql = "select * from quiz_history qh join quizzes q on q.quiz_id = qh.quiz_id where username = ?";
+
+        List<QuizHistory> activities = new ArrayList<>();
+
+        try (Connection conn = _source.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);) {
+
+            ps.setString(1, username);
+
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()) {
+                String user = rs.getString("username");
+                int score = rs.getInt("score");
+                int quiz_id = rs.getInt("quiz_id");
+                int completion_time_seconds = rs.getInt("completion_time_seconds");
+                String quiz_name = rs.getString("quiz_name");
+
+                QuizHistory quizActivity = new QuizHistory(user, score, quiz_id, completion_time_seconds, quiz_name);
+
+                activities.add(quizActivity);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return activities;
+    }
+
 }
