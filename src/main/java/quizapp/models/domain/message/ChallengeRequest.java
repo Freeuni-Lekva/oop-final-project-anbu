374c1794bd90a5fd6d5053f3f173b5c6611d96a8
atsin21 <atsin21@freeuni.edu.ge>
2023-08-21 18:49:39 +0400

    add score which will be retrieved from ChallengeManager later

--- src/main/java/quizapp/models/domain/message/ChallengeRequest.java
+++ src/main/java/quizapp/models/domain/message/ChallengeRequest.java
@@ -2,13 +2,17 @@ package quizapp.models.domain.message;
 
 public class ChallengeRequest extends Message {
     private final int quiz_id;
+    private final int score;
 
-    public ChallengeRequest(String sender, String receiver, int quiz_id) {
+    public ChallengeRequest(String sender, String receiver, int quiz_id, int score) {
         super(sender, receiver, MessageType.CHALLENGE);
         this.quiz_id = quiz_id;
+        this.score = score;
     }
 
     public int getQuizId() {
         return this.quiz_id;
     }
+
+    public int getScore() {return this.score;}
 }
