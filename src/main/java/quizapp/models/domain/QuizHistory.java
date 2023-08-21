a68534875c7dc3da81515b5722c651939a4877c1
atsin21 <atsin21@freeuni.edu.ge>
2023-08-21 06:27:22 +0400

    add quiz_name field and its getter and setter

--- src/main/java/quizapp/models/domain/QuizHistory.java
+++ src/main/java/quizapp/models/domain/QuizHistory.java
@@ -1,24 +1,26 @@
 package quizapp.models.domain;
 
 public class QuizHistory {
-    public String user;
+    public String username;
     public int quiz_id;
     public int score;
     public int completion_time;
+    public String quiz_name;
 
-    public QuizHistory(String user, int quiz_id, int score, int completion_time){
+    public QuizHistory(String username, int quiz_id, int score, int completion_time, String quiz_name){
         this.quiz_id = quiz_id;
-        this.user = user;
+        this.username = username;
         this.score = score;
         this.completion_time = completion_time;
+        this.quiz_name = quiz_name;
     }
 
-    public String getUser() {
-        return user;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public int get_quiz_id() {
@@ -43,5 +45,13 @@ public class QuizHistory {
 
     public void set_completion_time(int completion_time) {
         this.completion_time = completion_time;
+    }
+
+    public String get_quiz_name() {
+        return quiz_name;
+    }
+
+    public void set_quiz_name(String quiz_name) {
+        this.quiz_name = quiz_name;
     }
 }
